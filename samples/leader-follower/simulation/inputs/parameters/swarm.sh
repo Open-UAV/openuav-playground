105f933bc8b5255cfaf721a83fd08bf73d3b3031
jdas <jnaneshwar.das@gmail.com>
2018-01-08 14:24:52 -0500

    adding samples

---
+++ samples/leader-follower/simulation/inputs/parameters/swarm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source /root/.bashrc
+num_uavs=6
+duration_seconds=3000
+FOLLOW_D_GAIN=1
+LOOP_EDGE=40
+DIST_THRESHOLD=4
+ALTITUDE=40
+
---
+++ samples/leader-follower/simulation/test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+source /root/.profile 
+rostopic list 

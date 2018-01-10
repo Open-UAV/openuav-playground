d32c321d3b5fc8825174b832d821b2343887f95e
Abhijeet Singh <abhijeet060@gmail.com>
2018-01-10 16:03:37 -0500

    adding ajax when loading console

--- openuav-app/sim/urls.py
+++ openuav-app/sim/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
 	path('console/', views.console, name='console'),
+	path('console1/', views.console1, name='console1'),
+	path('console2/', views.console2, name='console2'),
 	path('unsecure_console/', views.unsecure_console, name='unsecure_console'),
-    	path('', views.index, name='index'),
+	path('unsecure_console1/', views.unsecure_console1, name='unsecure_console1'),
+	path('unsecure_console2/', views.unsecure_console2, name='unsecure_console2'),
+    path('', views.index, name='index'),
 ]
--- openuav-app/sim/views.py
+++ openuav-app/sim/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 import urllib.request
 import time
 import subprocess
+from django.views.decorators.csrf import csrf_exempt
 
 SIM_CONTAINER_PORT = '31819'
 PROJECT_PREFIX = 'openuavapp_'
@@ -17,7 +18,7 @@ def index(request):
 
 # docker run -it --net=openuavapp_default --name=openauv2 -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/abhijeet/Documents/openuav/samples/leader-follower/simulation:/simulation -e DISPLAY=$DISPLAY --entrypoint "/simulation/run_this.sh" openuavproject_openuav
 
-# nvidia-docker run -it --net=openuavapp_default --name=openuavapp_openauv3 -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/abhsingh/Laptop/openuav/samples/leader-follower/simulation:/simulation -e DISPLAY=:0 --entrypoint "/simulation/run_this.sh" openuavproject_openuav
+# nvidia-docker run -it --net=openuavapp_default --name=openuavapp_openauv3 -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/jdas/openuav-playground/samples/leader-follower:/simulation -e DISPLAY=:0 --entrypoint "/home/setup.sh" openuavapp_openuav
 
 def hostnameToIP(hostname):
 	cmd = ''' nslookup hostname | sed -n '6p' | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'  '''
@@ -91,5 +92,108 @@ def unsecure_console(request):
 
 		# num_uavs = 2
 		return HttpResponse(render(request, 'sim/dev_console_unsecure.html', {'range' : range(int(num_uavs)), 'num_uavs' : num_uavs, 'viewDomainName' : simulation_viewDomainName, 'rosDomainName' : simulation_rosDomainName}))
+	except Exception as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : str(e)}))
+
+@csrf_exempt
+def console1(request):
+	num_uav_str=''
+	measuresUp = 0
+
+	try:
+		simulation_hostname = PROJECT_PREFIX + request.GET.get('user','openuav_1')
+		simulation_ip = hostnameToIP(simulation_hostname)
+		simulation_viewDomainName = 'view-' + ipToViewNum(simulation_ip) + '.openuav.us'
+		simulation_rosDomainName = 'ros-' + ipToViewNum(simulation_ip) + '.openuav.us'
+	except Exception as e:	
+		return HttpResponse(render(request, 'sim/error.html', {'error' : '400 Bad Request' + str(e)}))
+
+	try:
+		while num_uav_str=='':
+			results = urllib.request.urlopen('http://' + simulation_ip + ':' + SIM_CONTAINER_PORT + '/query/numUavs').read()
+			num_uav_str=str(results.decode('UTF-8').split('#')[0])
+			time.sleep(1)
+		num_uavs = int(num_uav_str)
+		return HttpResponse(render(request, 'sim/dev_console_first.html', {'range' : range(int(num_uavs)), 'num_uavs' : num_uavs, 'viewDomainName' : simulation_viewDomainName, 'rosDomainName' : simulation_rosDomainName}))
+	except Exception as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : '500 Internal Server Error. Contact the admin.' + str(e)}))
+
+@csrf_exempt
+def console2(request):
+	num_uav_str=''
+	measuresUp = 0
+
+	try:
+		simulation_hostname = PROJECT_PREFIX + request.GET.get('user','openuav_1')
+		# simulation_hostname = 'openuavproject_openuav_1'
+		simulation_ip = hostnameToIP(simulation_hostname)
+		# simulation_ip = '172.28.0.3'
+		simulation_viewDomainName = 'view-' + ipToViewNum(simulation_ip) + '.openuav.us'
+		simulation_rosDomainName = 'ros-' + ipToViewNum(simulation_ip) + '.openuav.us'
+	except Exception as e:	
+		return HttpResponse(render(request, 'sim/error.html', {'error' : '400 Bad Request' + str(e)}))
+
+	try:
+		while num_uav_str=='':
+			results = urllib.request.urlopen('http://' + simulation_ip + ':' + SIM_CONTAINER_PORT + '/query/numUavs').read()
+			num_uav_str=str(results.decode('UTF-8').split('#')[0])
+			time.sleep(1)
+		num_uavs = int(num_uav_str)
+		while measuresUp<2:
+			results = urllib.request.urlopen('http://' + simulation_ip + ':' + SIM_CONTAINER_PORT + '/query/measures').read()
+			measuresUp=int(str(results.decode('UTF-8').split('#')[0]))
+			time.sleep(1)
+		time.sleep(2)
+		# num_uavs = 2
+		return HttpResponse(render(request, 'sim/dev_console_second.html', {'range' : range(int(num_uavs)), 'num_uavs' : num_uavs, 'viewDomainName' : simulation_viewDomainName, 'rosDomainName' : simulation_rosDomainName}))
+	except Exception as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : '500 Internal Server Error. Contact the admin.' + str(e)}))
+
+@csrf_exempt
+def unsecure_console1(request):
+	try:
+		simulation_hostname = PROJECT_PREFIX + request.GET.get('user','openuav_1')
+		# simulation_hostname = 'openuavproject_openuav_1'
+		simulation_ip = hostnameToIP(simulation_hostname)
+		# simulation_ip = '172.19.0.3'
+		simulation_viewDomainName = simulation_ip+ ':80'
+		simulation_rosDomainName = simulation_ip + ':9090'
+		num_uav_str=''
+		measuresUp = 0
+
+		while num_uav_str=='':
+			results = urllib.request.urlopen('http://' + simulation_ip + ':' + SIM_CONTAINER_PORT + '/query/numUavs').read()
+			num_uav_str=str(results.decode('UTF-8').split('#')[0])
+			time.sleep(1)
+		num_uavs = int(num_uav_str)
+		return HttpResponse(render(request, 'sim/dev_console_unsecure_first.html', {'range' : range(int(num_uavs)), 'num_uavs' : num_uavs, 'viewDomainName' : simulation_viewDomainName, 'rosDomainName' : simulation_rosDomainName}))
+	except Exception as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : str(e)}))
+
+@csrf_exempt
+def unsecure_console2(request):
+	try:
+		simulation_hostname = PROJECT_PREFIX + request.GET.get('user','openuav_1')
+		# simulation_hostname = 'openuavproject_openuav_1'
+		simulation_ip = hostnameToIP(simulation_hostname)
+		# simulation_ip = '172.19.0.3'
+		simulation_viewDomainName = simulation_ip+ ':80'
+		simulation_rosDomainName = simulation_ip + ':9090'
+		num_uav_str=''
+		measuresUp = 0
+
+		while num_uav_str=='':
+			results = urllib.request.urlopen('http://' + simulation_ip + ':' + SIM_CONTAINER_PORT + '/query/numUavs').read()
+			num_uav_str=str(results.decode('UTF-8').split('#')[0])
+			time.sleep(1)
+		num_uavs = int(num_uav_str)
+		while measuresUp<2:
+			results = urllib.request.urlopen('http://' + simulation_ip + ':' + SIM_CONTAINER_PORT + '/query/measures').read()
+			measuresUp=int(str(results.decode('UTF-8').split('#')[0]))
+			time.sleep(1)
+		time.sleep(2)
+
+		# num_uavs = 2
+		return HttpResponse(render(request, 'sim/dev_console_unsecure_second.html', {'range' : range(int(num_uavs)), 'num_uavs' : num_uavs, 'viewDomainName' : simulation_viewDomainName, 'rosDomainName' : simulation_rosDomainName}))
 	except Exception as e:
 		return HttpResponse(render(request, 'sim/error.html', {'error' : str(e)}))

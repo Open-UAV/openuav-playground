5e5862ff32174df7a5bd21fdb731d56b07fd00a1
Abhijeet Singh <abhijeet060@gmail.com>
2018-01-10 21:29:49 -0500

    refactored sim/views.py

--- openuav-app/sim/views.py
+++ openuav-app/sim/views.py
@@ -4,9 +4,11 @@ import urllib.request
 import time
 import subprocess
 from django.views.decorators.csrf import csrf_exempt
+from sim.exceptions import *
 
 SIM_CONTAINER_PORT = '31819'
 PROJECT_PREFIX = 'openuavapp_'
+ERROR_LEVEL = 2
 
 def index(request):
 	return HttpResponse("Hey!! What are you up to?")
@@ -30,171 +32,201 @@ def hostnameToIP(hostname):
 		stdin=p2.stdout,stdout=subprocess.PIPE)
 	p2.stdout.close()
 	output = p3.communicate()[0]
-	return output.decode('UTF-8').strip()
+	outputStr = output.decode('UTF-8').strip()
+
+	if outputStr == '':
+		raise NoContainerExc('nslookup returns empty')
+
+	return outputStr
 
 def ipToViewNum(ip):
 	# 172.28.0.5 corresponds to view-1.openuav.us - we return the number '1' here
-	lastOctetStr = ip.split('.')[-1]
-	lastOctetInt = int(lastOctetStr)
-	return str(lastOctetInt - 4)
+	try:
+		lastOctetStr = ip.split('.')[-1]
+		lastOctetInt = int(lastOctetStr)
+		return str(lastOctetInt - 4)
+	except Exception as e:
+		raise InvalidIPExc(str(e))
+
+def getUserIDWithoutDefault(request):
+	try:
+		userid = request.GET.get('user')
+		return userid
+	except Exception as e:
+		raise NoUserIDExc(str(e))
+
+def getUserIDWithDefault(request):
+	userid = request.GET.get('user','openuav_1')
+	return userid
+
+def getNumUAVs(simulation_ip):
+	try:
+		num_uav_str=''
+		while num_uav_str=='':
+			results = urllib.request.urlopen('http://' + simulation_ip + ':' + SIM_CONTAINER_PORT + '/query/numUavs').read()
+			num_uav_str=str(results.decode('UTF-8').split('#')[0])
+			if num_uav_str=='':
+				time.sleep(1)
+		num_uavs = int(num_uav_str)
+		return num_uavs
+	except Exception as e:
+		raise ContainerInformationFetchExc(str(e))
+
+def isSimReady(simulation_ip):
+	measuresUp = 0
+	try:
+		while measuresUp < 2:
+			results = urllib.request.urlopen('http://' + simulation_ip + ':' + SIM_CONTAINER_PORT + '/query/measures').read()
+			measuresUp=int(str(results.decode('UTF-8').split('#')[0]))
+			if measuresUp < 2:
+				time.sleep(1)
+		time.sleep(3)
+		return True
+	except Exception as e:
+		raise ContainerInformationFetchExc(str(e))
+
+def getErrorBasedOnLevel(str, e):
+	if ERROR_LEVEL == 2:
+		return str + '\nDebug: ' + e
+	elif ERROR_LEVEL == 1:
+		return str
+	else:
+		return 'Some error occured. Contact the admin.'
+
+def getSecureDomainNames(simNodeIP):
+	vdn = 'view-' + ipToViewNum(simNodeIP) + '.openuav.us'
+	rdn = 'ros-' + ipToViewNum(simNodeIP) + '.openuav.us'
+	return vdn, rdn
+
+def getUnsecureDomainNames(simNodeIP):
+	vdn = simNodeIP + ':80'
+	rdn = simNodeIP + ':9090'
+	return vdn, rdn
+
+########################################################
+########################################################
+########################################################
+########################################################
 
 
 def console(request):
-	num_uav_str=''
-	measuresUp = 0
-
 	try:
-		simulation_hostname = PROJECT_PREFIX + request.GET.get('user','openuav_1')
-		# simulation_hostname = 'openuavproject_openuav_1'
-		simulation_ip = hostnameToIP(simulation_hostname)
-		# simulation_ip = '172.28.0.3'
-		simulation_viewDomainName = 'view-' + ipToViewNum(simulation_ip) + '.openuav.us'
-		simulation_rosDomainName = 'ros-' + ipToViewNum(simulation_ip) + '.openuav.us'
+		userid = getUserIDWithoutDefault(request)
+		simNodeHostname = PROJECT_PREFIX + userid
+		simNodeIP = hostnameToIP(simNodeHostname)
+		viewDomainName, rosDomainName = getSecureDomainNames(simNodeIP)
+		numUAVs = getNumUAVs(simNodeIP)
+		isSimReady(simNodeIP)
+		return HttpResponse(render(request, 'sim/dev_console.html', {'range' : range(int(numUAVs)), 'num_uavs' : numUAVs, 'viewDomainName' : viewDomainName, 'rosDomainName' : rosDomainName}))
+	except NoContainerExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Container not present.', str(e))}))
+	except ContainerInformationFetchExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Simulation node not responding or responding incorrectly.', str(e))}))
+	except NoUserIDExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('No User ID present.', str(e))}))
+	except InvalidIPExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Invalid IP.', str(e))}))
 	except Exception as e:	
-		return HttpResponse(render(request, 'sim/error.html', {'error' : '400 Bad Request' + str(e)}))
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Internal Server Error.', str(e))}))
 
-	try:
-		while num_uav_str=='':
-			results = urllib.request.urlopen('http://' + simulation_ip + ':' + SIM_CONTAINER_PORT + '/query/numUavs').read()
-			num_uav_str=str(results.decode('UTF-8').split('#')[0])
-			time.sleep(1)
-		num_uavs = int(num_uav_str)
-		while measuresUp<2:
-			results = urllib.request.urlopen('http://' + simulation_ip + ':' + SIM_CONTAINER_PORT + '/query/measures').read()
-			measuresUp=int(str(results.decode('UTF-8').split('#')[0]))
-			time.sleep(1)
-
-		# num_uavs = 2
-		return HttpResponse(render(request, 'sim/dev_console.html', {'range' : range(int(num_uavs)), 'num_uavs' : num_uavs, 'viewDomainName' : simulation_viewDomainName, 'rosDomainName' : simulation_rosDomainName}))
-	except Exception as e:
-		return HttpResponse(render(request, 'sim/error.html', {'error' : '500 Internal Server Error. Contact the admin.' + str(e)}))
 
 def unsecure_console(request):
 	try:
-		simulation_hostname = PROJECT_PREFIX + request.GET.get('user','openuav_1')
-		# simulation_hostname = 'openuavproject_openuav_1'
-		simulation_ip = hostnameToIP(simulation_hostname)
-		# simulation_ip = '172.19.0.3'
-		simulation_viewDomainName = simulation_ip+ ':80'
-		simulation_rosDomainName = simulation_ip + ':9090'
-		num_uav_str=''
-		measuresUp = 0
-
-		while num_uav_str=='':
-			results = urllib.request.urlopen('http://' + simulation_ip + ':' + SIM_CONTAINER_PORT + '/query/numUavs').read()
-			num_uav_str=str(results.decode('UTF-8').split('#')[0])
-			time.sleep(1)
-		num_uavs = int(num_uav_str)
-		while measuresUp<2:
-			results = urllib.request.urlopen('http://' + simulation_ip + ':' + SIM_CONTAINER_PORT + '/query/measures').read()
-			measuresUp=int(str(results.decode('UTF-8').split('#')[0]))
-			time.sleep(1)
-
-		# num_uavs = 2
-		return HttpResponse(render(request, 'sim/dev_console_unsecure.html', {'range' : range(int(num_uavs)), 'num_uavs' : num_uavs, 'viewDomainName' : simulation_viewDomainName, 'rosDomainName' : simulation_rosDomainName}))
-	except Exception as e:
-		return HttpResponse(render(request, 'sim/error.html', {'error' : str(e)}))
-
-@csrf_exempt
-def console1(request):
-	num_uav_str=''
-	measuresUp = 0
-
-	try:
-		simulation_hostname_without_prefix = request.GET.get('user','openuav_1')
-		simulation_hostname = PROJECT_PREFIX + simulation_hostname_without_prefix
-		simulation_ip = hostnameToIP(simulation_hostname)
-		simulation_viewDomainName = 'view-' + ipToViewNum(simulation_ip) + '.openuav.us'
-		simulation_rosDomainName = 'ros-' + ipToViewNum(simulation_ip) + '.openuav.us'
+		userid = getUserIDWithDefault(request)
+		simNodeHostname = PROJECT_PREFIX + userid
+		simNodeIP = hostnameToIP(simNodeHostname)
+		viewDomainName, rosDomainName = getUnsecureDomainNames(simNodeIP)
+		numUAVs = getNumUAVs(simNodeIP)
+		isSimReady(simNodeIP)
+		return HttpResponse(render(request, 'sim/dev_console_unsecure.html', {'range' : range(int(numUAVs)), 'num_uavs' : numUAVs, 'viewDomainName' : viewDomainName, 'rosDomainName' : rosDomainName}))
+	except NoContainerExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Container not present.', str(e))}))
+	except ContainerInformationFetchExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Simulation node not responding or responding incorrectly.', str(e))}))
+	except NoUserIDExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('No User ID present.', str(e))}))
+	except InvalidIPExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Invalid IP.', str(e))}))
 	except Exception as e:	
-		return HttpResponse(render(request, 'sim/error.html', {'error' : '400 Bad Request' + str(e)}))
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Internal Server Error.', str(e))}))
 
+def console1(request):
 	try:
-		while num_uav_str=='':
-			results = urllib.request.urlopen('http://' + simulation_ip + ':' + SIM_CONTAINER_PORT + '/query/numUavs').read()
-			num_uav_str=str(results.decode('UTF-8').split('#')[0])
-			time.sleep(1)
-		num_uavs = int(num_uav_str)
-		return HttpResponse(render(request, 'sim/dev_console_first.html', {'userid': simulation_hostname_without_prefix, 'range' : range(int(num_uavs)), 'num_uavs' : num_uavs, 'viewDomainName' : simulation_viewDomainName, 'rosDomainName' : simulation_rosDomainName}))
-	except Exception as e:
-		return HttpResponse(render(request, 'sim/error.html', {'error' : '500 Internal Server Error. Contact the admin.' + str(e)}))
+		userid = getUserIDWithoutDefault(request)
+		simNodeHostname = PROJECT_PREFIX + userid
+		simNodeIP = hostnameToIP(simNodeHostname)
+		viewDomainName, rosDomainName = getSecureDomainNames(simNodeIP)
+		numUAVs = getNumUAVs(simNodeIP)
+		return HttpResponse(render(request, 'sim/dev_console_first.html', {'userid': userid, 'range' : range(int(numUAVs)), 'num_uavs' : numUAVs, 'viewDomainName' : viewDomainName, 'rosDomainName' : rosDomainName}))
+	except NoContainerExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Container not present.', str(e))}))
+	except ContainerInformationFetchExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Simulation node not responding or responding incorrectly.', str(e))}))
+	except NoUserIDExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('No User ID present.', str(e))}))
+	except InvalidIPExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Invalid IP.', str(e))}))
+	except Exception as e:	
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Internal Server Error.', str(e))}))
 
 @csrf_exempt
 def console2(request):
-	num_uav_str=''
-	measuresUp = 0
-
 	try:
-		simulation_hostname = PROJECT_PREFIX + request.GET.get('user','openuav_1')
-		# simulation_hostname = 'openuavproject_openuav_1'
-		simulation_ip = hostnameToIP(simulation_hostname)
-		# simulation_ip = '172.28.0.3'
-		simulation_viewDomainName = 'view-' + ipToViewNum(simulation_ip) + '.openuav.us'
-		simulation_rosDomainName = 'ros-' + ipToViewNum(simulation_ip) + '.openuav.us'
+		userid = getUserIDWithoutDefault(request)
+		simNodeHostname = PROJECT_PREFIX + userid
+		simNodeIP = hostnameToIP(simNodeHostname)
+		viewDomainName, rosDomainName = getSecureDomainNames(simNodeIP)
+		numUAVs = getNumUAVs(simNodeIP)
+		isSimReady(simNodeIP)
+		return HttpResponse(render(request, 'sim/dev_console_second.html', {'range' : range(int(numUAVs)), 'num_uavs' : numUAVs, 'viewDomainName' : viewDomainName, 'rosDomainName' : rosDomainName}))
+	except NoContainerExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Container not present.', str(e))}))
+	except ContainerInformationFetchExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Simulation node not responding or responding incorrectly.', str(e))}))
+	except NoUserIDExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('No User ID present.', str(e))}))
+	except InvalidIPExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Invalid IP.', str(e))}))
 	except Exception as e:	
-		return HttpResponse(render(request, 'sim/error.html', {'error' : '400 Bad Request' + str(e)}))
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Internal Server Error.', str(e))}))
 
-	try:
-		while num_uav_str=='':
-			results = urllib.request.urlopen('http://' + simulation_ip + ':' + SIM_CONTAINER_PORT + '/query/numUavs').read()
-			num_uav_str=str(results.decode('UTF-8').split('#')[0])
-			time.sleep(1)
-		num_uavs = int(num_uav_str)
-		while measuresUp<2:
-			results = urllib.request.urlopen('http://' + simulation_ip + ':' + SIM_CONTAINER_PORT + '/query/measures').read()
-			measuresUp=int(str(results.decode('UTF-8').split('#')[0]))
-			time.sleep(1)
-		time.sleep(2)
-		# num_uavs = 2
-		return HttpResponse(render(request, 'sim/dev_console_second.html', {'range' : range(int(num_uavs)), 'num_uavs' : num_uavs, 'viewDomainName' : simulation_viewDomainName, 'rosDomainName' : simulation_rosDomainName}))
-	except Exception as e:
-		return HttpResponse(render(request, 'sim/error.html', {'error' : '500 Internal Server Error. Contact the admin.' + str(e)}))
-
-@csrf_exempt
 def unsecure_console1(request):
 	try:
-		simulation_hostname_without_prefix = request.GET.get('user','openuav_1')
-		simulation_hostname = PROJECT_PREFIX + simulation_hostname_without_prefix
-		simulation_ip = hostnameToIP(simulation_hostname)
-		# simulation_ip = '172.19.0.3'
-		simulation_viewDomainName = simulation_ip+ ':80'
-		simulation_rosDomainName = simulation_ip + ':9090'
-		num_uav_str=''
-		measuresUp = 0
-
-		while num_uav_str=='':
-			results = urllib.request.urlopen('http://' + simulation_ip + ':' + SIM_CONTAINER_PORT + '/query/numUavs').read()
-			num_uav_str=str(results.decode('UTF-8').split('#')[0])
-			time.sleep(1)
-		num_uavs = int(num_uav_str)
-		return HttpResponse(render(request, 'sim/dev_console_unsecure_first.html', {'userid': simulation_hostname_without_prefix, 'range' : range(int(num_uavs)), 'num_uavs' : num_uavs, 'viewDomainName' : simulation_viewDomainName, 'rosDomainName' : simulation_rosDomainName}))
-	except Exception as e:
-		return HttpResponse(render(request, 'sim/error.html', {'error' : str(e)}))
+		userid = getUserIDWithDefault(request)
+		simNodeHostname = PROJECT_PREFIX + userid
+		simNodeIP = hostnameToIP(simNodeHostname)
+		viewDomainName, rosDomainName = getUnsecureDomainNames(simNodeIP)
+		numUAVs = getNumUAVs(simNodeIP)
+		return HttpResponse(render(request, 'sim/dev_console_unsecure_first.html', {'userid': userid, 'range' : range(int(numUAVs)), 'num_uavs' : numUAVs, 'viewDomainName' : viewDomainName, 'rosDomainName' : rosDomainName}))
+	except NoContainerExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Container not present.', str(e))}))
+	except ContainerInformationFetchExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Simulation node not responding or responding incorrectly.', str(e))}))
+	except NoUserIDExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('No User ID present.', str(e))}))
+	except InvalidIPExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Invalid IP.', str(e))}))
+	except Exception as e:	
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Internal Server Error.', str(e))}))
 
 @csrf_exempt
 def unsecure_console2(request):
 	try:
-		simulation_hostname = PROJECT_PREFIX + request.GET.get('user','openuav_1')
-		# simulation_hostname = 'openuavproject_openuav_1'
-		simulation_ip = hostnameToIP(simulation_hostname)
-		# simulation_ip = '172.19.0.3'
-		simulation_viewDomainName = simulation_ip+ ':80'
-		simulation_rosDomainName = simulation_ip + ':9090'
-		num_uav_str=''
-		measuresUp = 0
+		userid = getUserIDWithDefault(request)
+		simNodeHostname = PROJECT_PREFIX + userid
+		simNodeIP = hostnameToIP(simNodeHostname)
+		viewDomainName, rosDomainName = getUnsecureDomainNames(simNodeIP)
+		numUAVs = getNumUAVs(simNodeIP)
+		isSimReady(simNodeIP)
+		return HttpResponse(render(request, 'sim/dev_console_unsecure_second.html', {'range' : range(int(numUAVs)), 'num_uavs' : numUAVs, 'viewDomainName' : viewDomainName, 'rosDomainName' : rosDomainName}))
+	except NoContainerExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Container not present.', str(e))}))
+	except ContainerInformationFetchExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Simulation node not responding or responding incorrectly.', str(e))}))
+	except NoUserIDExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('No User ID present.', str(e))}))
+	except InvalidIPExc as e:
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Invalid IP.', str(e))}))
+	except Exception as e:	
+		return HttpResponse(render(request, 'sim/error.html', {'error' : getErrorBasedOnLevel('Internal Server Error.', str(e))}))
 
-		while num_uav_str=='':
-			results = urllib.request.urlopen('http://' + simulation_ip + ':' + SIM_CONTAINER_PORT + '/query/numUavs').read()
-			num_uav_str=str(results.decode('UTF-8').split('#')[0])
-			time.sleep(1)
-		num_uavs = int(num_uav_str)
-		while measuresUp<2:
-			results = urllib.request.urlopen('http://' + simulation_ip + ':' + SIM_CONTAINER_PORT + '/query/measures').read()
-			measuresUp=int(str(results.decode('UTF-8').split('#')[0]))
-			time.sleep(1)
-		time.sleep(2)
-
-		# num_uavs = 2
-		return HttpResponse(render(request, 'sim/dev_console_unsecure_second.html', {'range' : range(int(num_uavs)), 'num_uavs' : num_uavs, 'viewDomainName' : simulation_viewDomainName, 'rosDomainName' : simulation_rosDomainName}))
-	except Exception as e:
-		return HttpResponse(render(request, 'sim/error.html', {'error' : str(e)}))

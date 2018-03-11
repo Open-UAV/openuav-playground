2036f0ff7abb6dadce16d15547bed9f18ac9a240
darknight-007 <jnaneshwar.das@gmail.com>
2018-03-11 15:13:48 -0400

    refactoring swarm spawn and launch sequence

--- samples/formation/run_this.sh
+++ samples/formation/run_this.sh
@@ -21,31 +21,37 @@ python /simulation/inputs/setup/swarm.py $num_uavs &> /dev/null &
 python /simulation/inputs/setup/sitl-mavros.py $num_uavs &> /dev/null &
 sleep 1
 
+roslaunch px4 posix_sitl_multi_gazebo_ros.launch &> /dev/null &
+sleep 20
+
+roslaunch px4 posix_sitl_multi_sitl_mavros.launch &> /dev/null &
+sleep 10
+
 
 echo "Launch UAVs" >> /tmp/debug.log
 
-#for((i = 0;i<$num_uavs;i+=1))
-#do
-#    python /simulation/inputs/controllers/simple_Formation.py $i $num_uavs $FOLLOW_D_GAIN &> /simulation/outputs/patroLog$i.txt &
-#    sleep 6
-#done
-#echo "Launch Sequencer" >> /tmp/debug.log
-#python /simulation/inputs/controllers/sequencer.py $num_uavs &> /simulation/outputs/sequencerLog.txt &
-#
-#
-#roslaunch rosbridge_server rosbridge_websocket.launch ssl:=false &> /dev/null &
-#python /simulation/inputs/measures/measureInterRobotDistance.py $num_uavs 1 &> /dev/null &
-#rosrun web_video_server web_video_server _port:=80 _server_threads:=100 &> /dev/null &
-#tensorboard --logdir=/simulation/outputs/ --port=8008 &> /dev/null &
-#roslaunch opencv_apps general_contours.launch  image:=/uav_2_camera/image_raw_front debug_view:=false &> /dev/null &
-#
-#echo "Measures..." >> /tmp/debug.log
-#for((i=1;i<=$num_uavs;i+=1))
-#do
-#        /usr/bin/python -u /opt/ros/kinetic/bin/rostopic echo -p /mavros$i/local_position/odom > /simulation/outputs/uav$i.csv &
-#    done
-#    /usr/bin/python -u /opt/ros/kinetic/bin/rostopic echo -p /measure > /simulation/outputs/measure.csv &
-#
+for((i = 0;i<$num_uavs;i+=1))
+do
+    python /simulation/inputs/controllers/simple_Formation.py $i $num_uavs $FOLLOW_D_GAIN &> /simulation/outputs/patroLog$i.txt &
+    sleep 6
+done
+echo "Launch Sequencer" >> /tmp/debug.log
+python /simulation/inputs/controllers/sequencer.py $num_uavs &> /simulation/outputs/sequencerLog.txt &
+
+
+roslaunch rosbridge_server rosbridge_websocket.launch ssl:=false &> /dev/null &
+python /simulation/inputs/measures/measureInterRobotDistance.py $num_uavs 1 &> /dev/null &
+rosrun web_video_server web_video_server _port:=80 _server_threads:=100 &> /dev/null &
+tensorboard --logdir=/simulation/outputs/ --port=8008 &> /dev/null &
+roslaunch opencv_apps general_contours.launch  image:=/uav_2_camera/image_raw_front debug_view:=false &> /dev/null &
+
+echo "Measures..." >> /tmp/debug.log
+for((i=1;i<=$num_uavs;i+=1))
+do
+        /usr/bin/python -u /opt/ros/kinetic/bin/rostopic echo -p /mavros$i/local_position/odom > /simulation/outputs/uav$i.csv &
+    done
+    /usr/bin/python -u /opt/ros/kinetic/bin/rostopic echo -p /measure > /simulation/outputs/measure.csv &
+
 
     sleep $duration_seconds
     cat /simulation/outputs/measure.csv | awk -F',' '{sum+=$2; ++n} END { print sum/n }' > /simulation/outputs/average_measure.txt

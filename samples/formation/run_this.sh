1dd570585db12bccc5a4462b12e8340671a03a14
darknight-007 <jnaneshwar.das@gmail.com>
2018-03-11 21:32:01 -0400

    refactoring swarm spawn and launch sequence

--- samples/formation/run_this.sh
+++ samples/formation/run_this.sh
@@ -11,12 +11,12 @@ rm -f /root/src/Firmware/posix-configs/SITL/init/lpe/f450-tmp-*
 rm -f /root/src/Firmware/launch/posix_sitl_multi_tmp.launch
 
 ## world setup #
-cp /simulation/inputs/world/empty.world /root/src/Firmware/Tools/sitl_gazebo/worlds/empty.world
+#cp /simulation/inputs/world/empty.world /root/src/Firmware/Tools/sitl_gazebo/worlds/empty.world
 cp /simulation/inputs/models/f450-1/f450-1.sdf /root/src/Firmware/Tools/sitl_gazebo/models/f450-1/f450-1.sdf
 
 rm -f /simulation/outputs/*.csv
 rm -f /simulation/outputs/*.txt
-echo "Setup..." >> /tmp/debug.log
+echo "Setup..." >> /tmp/debug
 
 
 python /simulation/inputs/setup/gen_gazebo_ros_spawn.py $num_uavs 
@@ -30,12 +30,12 @@ for((i=1;i<=$num_uavs;i+=1))
 do
 echo "px4 posix_sitl_multi_gazebo_ros$num_uavs.launch"
     roslaunch px4 posix_sitl_multi_gazebo_ros$i.launch & /dev/null &
-echo "launching uav$i ..." >> /tmp/debug.log
+echo "launching uav$i ..." >> /tmp/debug
 sleep 5
 done
 
 
-echo "launching sitl(s)..." >> /tmp/debug.log
+echo "launching sitl(s)..." >> /tmp/debug
 roslaunch px4 posix_sitl_multi_px4_sitl.launch & /dev/null &
 sleep 5
 
@@ -43,14 +43,14 @@ echo "launching mavros(s) ..." >> /tmp/debug.log
 roslaunch px4 posix_sitl_multi_mavros.launch & /dev/null &
 sleep 5
 
-echo "Launch UAVs" >> /tmp/debug.log
+echo "Launch UAVs" >> /tmp/debug
 
 for((i = 0;i<$num_uavs;i+=1))
 do
     python /simulation/inputs/controllers/simple_Formation.py $i $num_uavs $FOLLOW_D_GAIN &> /simulation/outputs/patroLog$i.txt &
     sleep 1
 done
-echo "Launch Sequencer" >> /tmp/debug.log
+echo "Launch Sequencer" >> /tmp/debug
 python /simulation/inputs/controllers/sequencer.py $num_uavs &> /simulation/outputs/sequencerLog.txt &
 
 
@@ -60,7 +60,7 @@ rosrun web_video_server web_video_server _port:=80 _server_threads:=100 &> /dev/
 tensorboard --logdir=/simulation/outputs/ --port=8008 &> /dev/null &
 roslaunch opencv_apps general_contours.launch  image:=/uav_2_camera/image_raw_front debug_view:=false &> /dev/null &
 
-echo "Measures..." >> /tmp/debug.log
+echo "Measures..." >> /tmp/debug
 for((i=1;i<=$num_uavs;i+=1))
 do
         /usr/bin/python -u /opt/ros/kinetic/bin/rostopic echo -p /mavros$i/local_position/odom > /simulation/outputs/uav$i.csv &

1bc06fa9d5f2bbaa50c65dc0119324755c7bcaae
Jnaneshwar Das <jnaneshwar.das@gmail.com>
2018-04-11 07:03:47 -0400

    merge conflict fixed

--- samples/formation/inputs/parameters/swarm.sh
+++ samples/formation/inputs/parameters/swarm.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 source /root/.bashrc
-<<<<<<< HEAD
-num_uavs=10
-=======
-num_uavs=5
->>>>>>> bbdfbdc0738d83bd40a23bbea19763014d7bfbca
+num_uavs=4
 duration_seconds=3000
 FOLLOW_D_GAIN=1
 LOOP_EDGE=40

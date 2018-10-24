38c749c4469f733ff6ec17ca079f496968d0df75
Harish Anand <harishanand95@gmail.com>
2018-10-24 01:09:14 -0700

    Fix: py2 string operations removed
    Signed-off-by: Harish Anand

--- openuav-app/sim/views.py
+++ openuav-app/sim/views.py
@@ -286,8 +286,8 @@ def debugStmts(request):
 
 		results = urllib.request.urlopen('http://' + simulation_ip + ':' + SIM_CONTAINER_PORT + '/query/debugStmts').read()
 		debugStatements = str(results.decode('UTF-8'))
-		debugStatements = string.replace(debugStatements, '\r\n', '<br />')
-		debugStatements = string.replace(debugStatements, '\n', '<br />')
+		debugStatements = debugStatements.replace('\r\n', '<br />')
+		debugStatements = debugStatements.replace('\n', '<br />')
 	except Exception as e:
 		print(e)
 

0ca4f967a07a2e3435324b2932eadfbf08480189
jdas <jnaneshwar.das@gmail.com>
2018-11-14 10:32:07 -0500

    cleaning file permissions for container, removing redundant apt updates from simulation container Dockerfile

--- openuav-app/dockerfiles/openuav_sample/setup.sh
+++ openuav-app/dockerfiles/openuav_sample/setup.sh
@@ -6,9 +6,9 @@ python3 /django/manage.py migrate &> /dev/null
 python3 /django/manage.py runserver 0.0.0.0:31819 &> /dev/null &
 #Django start server
 
-cd /wetty/wetty
-node app.js -p 3000 &> /dev/null &
-cd
+#cd /wetty/wetty
+#node app.js -p 3000 &> /dev/null &
+#cd
 
 ## Previous clean-up
 rm -rf /root/src/Firmware/Tools/sitl_gazebo/models/f450-tmp-*
--- samples/leader-follower/run_this.sh
+++ samples/leader-follower/run_this.sh
@@ -52,25 +52,24 @@ python /simulation/inputs/controllers/test_1_Loop.py $LOOP_EDGE $ALTITUDE 1 0 &>
 python /simulation/inputs/setup/testArmAll.py $num_uavs &> /dev/null &
 
 sleep 3
-
 for((i=1;i<$num_uavs;i+=1))
 do
     one=1
     python /simulation/inputs/controllers/test_3_Follow.py $(( i + one)) $i $FOLLOW_D_GAIN &> /dev/null &
     sleep 1
 done
+roslaunch opencv_apps general_contours.launch  image:=/uav_1_camera_front/image_raw debug_view:=false &> /dev/null &
 
 echo "Measures..."
 python /simulation/inputs/measures/measureInterRobotDistance.py $num_uavs 1 &> /dev/null &
-roslaunch opencv_apps general_contours.launch  image:=/uav_2_camera_front/image_raw debug_view:=false &> /dev/null &
+
 
 for((i=1;i<=$num_uavs;i+=1))
 do
         /usr/bin/python -u /opt/ros/kinetic/bin/rostopic echo -p /mavros$i/local_position/odom > /simulation/outputs/uav$i.csv &
     done
     /usr/bin/python -u /opt/ros/kinetic/bin/rostopic echo -p /measure > /simulation/outputs/measure.csv &
-
-
     sleep $duration_seconds
-    cat /simulation/outputs/measure.csv | awk -F',' '{sum+=$2; ++n} END { print sum/n }' > /simulation/outputs/average_measure.txt
+    
+cat /simulation/outputs/measure.csv | awk -F',' '{sum+=$2; ++n} END { print sum/n }' > /simulation/outputs/average_measure.txt
 

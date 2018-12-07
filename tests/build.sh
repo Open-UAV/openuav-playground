f90354b7204f5c6e8321fb5e93f3c77c72f2117f
Harish Anand <harishanand95@gmail.com>
2018-12-07 15:11:17 -0700

    Fix: test for networks created

--- tests/build.sh
+++ tests/build.sh
@@ -2,4 +2,7 @@
 cd ./tests
 docker-compose up --detach
 cd ../
+docker network ls
+docker images
+docker ps
 docker run -it --net=tests_openuavapp_default --name=openuavapp_x${3:-`date +%s`} -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/hanand/openuav-playground/samples/leader-follower:/simulation -e DISPLAY=:0 --entrypoint "/home/setup.sh" dreamslab/openuavapp_openuav:latest

2cf7dd80767c785d968d8319f5cb9e7f9892d75f
Harish Anand <harishanand95@gmail.com>
2018-12-07 12:50:43 -0700

    Fix: missing testsetup and build files

--- tests/build.sh
+++ tests/build.sh
@@ -1,1 +1,3 @@
 #!/usr/bin/env bash
+cd ./tests
+docker-compose up --detach
--- tests/test_setup.sh
+++ tests/test_setup.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+sudo apt-get -y update
+sudo apt-get install -y software-properties-common screen
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
+sudo apt-get update -y
+sudo apt-get install docker-ce -y
+sudo usermod -aG docker $USER
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose

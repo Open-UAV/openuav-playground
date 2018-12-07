56a66d95ad280a27dc8050d3ec87b61cf85f9919
Harish Anand <harishanand95@gmail.com>
2018-12-07 13:05:19 -0700

    fix stalling after docker-compose

--- tests/build.sh
+++ tests/build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 cd ./tests
 docker-compose up --detach
+cd ../
+docker run -it --net=openuavapp_default --name=openuavapp_x${3:-`date +%s`} -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/hanand/openuav-playground/samples/leader-follower:/simulation -e DISPLAY=:0 --entrypoint "/home/setup.sh" openuavapp_openuav

53bb28c88fde6c2e7c40b70ee97aa0d26df8686d
Harish Anand <harishanand95@gmail.com>
2018-12-07 16:34:08 -0700

    Check measure average value

--- tests/build.sh
+++ tests/build.sh
@@ -2,11 +2,5 @@
 cd ./tests
 docker-compose up --detach
 cd ../
-docker network ls
-docker images
-docker ps
-pwd
-node -v
-npm -v
-nodejs -v
 docker run -it --net=tests_default --name=openuavapp_x${3:-`date +%s`} -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/travis/build/harishanand95/openuav-playground/samples/leader-follower:/simulation -e DISPLAY=:0 --entrypoint "/home/setup.sh" dreamslab/openuavapp_openuav:latest
+node tests/measure.js

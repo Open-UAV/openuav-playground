dea503f42e5c2c5ef89f81ceb3616dd6395e05f4
Harish Anand <harishanand95@gmail.com>
2018-12-08 15:55:54 -0700

    Add travis CI for testing  (#19)
    * Created test directory
    
    * Added dockerfile for travis CI build
    
    * Fix: missing testsetup and build files
    
    * Fix: add dreamslab build repo
    
    * Fix: rename build dir to image
    
    * fix stalling after docker-compose
    
    * missing image name in build
    
    * Fix: missing tests in network name
    
    * Fix: test for networks created
    
    * Fix to test_default
    
    * Fix username missing
    
    * Fix: username set to travis
    
    * Missing repo name in path
    
    * Fix: check travis has node and npm
    
    * Check measure average value
    
    * Fix: remove interactive option from docker run
    
    * Fix: add roslib package
    
    * Fix: install roslib package globally
    
    * Fix: add network test_default
    
    * Fix: add measure error handler ERRCONNREFUSED
    
    * Fix stop measure js after 250 iterations
    
    * Fix: exit after 250 iterations
    
    * Fix: travis script added
    
    * Fix: change lang to generic
    
    * Fix: end with exit 0
    
    * Fix: change repo name to OpenUAV

---
+++ tests/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd ./tests
+docker-compose up --detach
+cd ../
+npm install roslib
+docker run -d --net=tests_default --name=openuavapp_x${3:-`date +%s`} -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/travis/build/Open-UAV/openuav-playground/samples/leader-follower:/simulation -e DISPLAY=:0 --entrypoint "/home/setup.sh" dreamslab/openuavapp_openuav:latest
---
+++ tests/measure.js
@@ -0,0 +1,29 @@
+var ROSLIB = require("roslib");
+function measure() {
+    ctr = 0
+    measureTotal = 0
+    websocket_url_str = 'ws://172.28.0.5:9090';
+    ros = new ROSLIB.Ros({
+        url: websocket_url_str
+    });
+    listenerMeasure = new ROSLIB.Topic({
+        ros: ros,
+        name: '/measure',
+        messageType: 'std_msgs/Float64'
+    });
+    listenerMeasure.subscribe(function(message) {
+            measureTotal = measureTotal + parseFloat(message.data)
+            ctr = ctr + 1;
+            measureMean = measureTotal / ctr;
+            console.log(measureMean);
+            if (ctr == 250){
+                console.log("Exiting after 250 iterations");
+                process.exit(0);
+            }
+    });
+    ros.on('error', function(error) {
+        console.log('Error connecting to websocket server: ', error.code);
+        setTimeout(measure, 5000);
+    });
+}
+measure();

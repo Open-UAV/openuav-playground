29d7720b85e7c60328251b1838ca1a561002ddf7
Harish Anand <harishanand95@gmail.com>
2018-12-07 20:33:24 -0700

    Fix: remove interactive option from docker run

--- tests/build.sh
+++ tests/build.sh
@@ -2,5 +2,5 @@
 cd ./tests
 docker-compose up --detach
 cd ../
-docker run -it --net=tests_default --name=openuavapp_x${3:-`date +%s`} -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/travis/build/harishanand95/openuav-playground/samples/leader-follower:/simulation -e DISPLAY=:0 --entrypoint "/home/setup.sh" dreamslab/openuavapp_openuav:latest
+docker run -d --net=tests_default --name=openuavapp_x${3:-`date +%s`} -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/travis/build/harishanand95/openuav-playground/samples/leader-follower:/simulation -e DISPLAY=:0 --entrypoint "/home/setup.sh" dreamslab/openuavapp_openuav:latest
 node tests/measure.js

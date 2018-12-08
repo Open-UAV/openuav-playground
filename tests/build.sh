e1eac23efe1fbd72da9e14f743fd106ad73eab18
Harish Anand <harishanand95@gmail.com>
2018-12-08 13:31:48 -0800

    Fix: change repo name to OpenUAV

--- tests/build.sh
+++ tests/build.sh
@@ -3,4 +3,4 @@ cd ./tests
 docker-compose up --detach
 cd ../
 npm install roslib
-docker run -d --net=tests_default --name=openuavapp_x${3:-`date +%s`} -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/travis/build/harishanand95/openuav-playground/samples/leader-follower:/simulation -e DISPLAY=:0 --entrypoint "/home/setup.sh" dreamslab/openuavapp_openuav:latest
+docker run -d --net=tests_default --name=openuavapp_x${3:-`date +%s`} -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/travis/build/Open-UAV/openuav-playground/samples/leader-follower:/simulation -e DISPLAY=:0 --entrypoint "/home/setup.sh" dreamslab/openuavapp_openuav:latest

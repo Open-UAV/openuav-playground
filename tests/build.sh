2e4943919992e27e5fcdfaece7adc5154d48d04f
Harish Anand <harishanand95@gmail.com>
2018-12-08 11:17:29 -0800

    Fix: add measure error handler ERRCONNREFUSED

--- tests/build.sh
+++ tests/build.sh
@@ -2,7 +2,6 @@
 cd ./tests
 docker-compose up --detach
 cd ../
-docker run -d --net=tests_default --name=openuavapp_x${3:-`date +%s`} -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/travis/build/harishanand95/openuav-playground/samples/leader-follower:/simulation -e DISPLAY=:0 --entrypoint "/home/setup.sh" dreamslab/openuavapp_openuav:latest
 npm install roslib
-docker network inspect tests_default
+docker run -d --net=tests_default --name=openuavapp_x${3:-`date +%s`} -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/travis/build/harishanand95/openuav-playground/samples/leader-follower:/simulation -e DISPLAY=:0 --entrypoint "/home/setup.sh" dreamslab/openuavapp_openuav:latest
 node tests/measure.js
--- tests/measure.js
+++ tests/measure.js
@@ -1,18 +1,25 @@
 var ROSLIB = require("roslib");
-ctr = 0
-measureTotal = 0
-websocket_url_str = 'ws://172.28.0.5:9090';
-ros = new ROSLIB.Ros({
-    url: websocket_url_str
-});
-listenerMeasure = new ROSLIB.Topic({
-    ros: ros,
-    name: '/measure',
-    messageType: 'std_msgs/Float64'
-});
-listenerMeasure.subscribe(function(message) {
-        measureTotal = measureTotal + parseFloat(message.data)
-        ctr = ctr + 1;
-        measureMean = measureTotal / ctr;
-        console.log(measureMean);
-});
+function measure() {
+    ctr = 0
+    measureTotal = 0
+    websocket_url_str = 'ws://172.28.0.5:9090';
+    ros = new ROSLIB.Ros({
+        url: websocket_url_str
+    });
+    listenerMeasure = new ROSLIB.Topic({
+        ros: ros,
+        name: '/measure',
+        messageType: 'std_msgs/Float64'
+    });
+    listenerMeasure.subscribe(function(message) {
+            measureTotal = measureTotal + parseFloat(message.data)
+            ctr = ctr + 1;
+            measureMean = measureTotal / ctr;
+            console.log(measureMean);
+    });
+    ros.on('error', function(error) {
+        console.log('Error connecting to websocket server: ', error.code);
+        setTimeout(measure, 5000);
+    });
+}
+measure();

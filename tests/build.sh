e84fc926a0ca6a6d654aba9eace90fc5495067a4
Harish Anand <harishanand95@gmail.com>
2018-12-08 00:02:59 -0800

    Fix: add roslib package

--- tests/build.sh
+++ tests/build.sh
@@ -3,4 +3,5 @@ cd ./tests
 docker-compose up --detach
 cd ../
 docker run -d --net=tests_default --name=openuavapp_x${3:-`date +%s`} -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/travis/build/harishanand95/openuav-playground/samples/leader-follower:/simulation -e DISPLAY=:0 --entrypoint "/home/setup.sh" dreamslab/openuavapp_openuav:latest
+npm install -g roslib
 node tests/measure.js
